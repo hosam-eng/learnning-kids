7a1f129807eb8f67364ba871a271e820a58ba31f
hosam-eng <55917685+hosam-eng@users.noreply.github.com>
2019-12-15 04:09:56 +0200

    Add files via upload

---
+++ version2/db.js
@@ -0,0 +1,26 @@
+
+
+	$.ajax({
+		"type":"GET",
+        "url":"index2.php",
+        "data":{"db":""},
+        "beforeSend":function(){
+          console.log("sending to server...");
+			
+		},
+        "success":function(response){
+				
+            console.log("server replayed");
+			var $db =JSON.parse(response);
+			$(".database").append("<table> <tr> <th>type</th> <th>targer</th> <th>date</th> </tr>");
+			
+			for($i=0;$i<$db.length;$i++){	
+			  	
+			$(".database").append("<tr> <td>"+  $db[$i].type+ "</td> <td>"+ $db[$i].targer +"</td> <td>"+
+              			$db[$i].date +"</td></tr></table>");
+			 
+			  
+			}
+     
+	}
+})
