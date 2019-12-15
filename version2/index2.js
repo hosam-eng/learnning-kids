5500ca8b9f9926d772271ae35f884eba23b2ac82
hosam-eng <55917685+hosam-eng@users.noreply.github.com>
2019-12-15 04:11:13 +0200

    Update index2.js

--- version2/index2.js
+++ version2/index2.js
@@ -118,6 +118,7 @@ btn.addEventListener("click",function(e)
    var charactersLength = characters.length;
 	  if(num>0 && num<27)
 	  {
+		  document.getElementById("error").innerHTML=null;
    for ( var i = 0; i < num; i++ ) 
    {
 	  var s=characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -133,6 +134,8 @@ btn.addEventListener("click",function(e)
         }
       }
     }
+	else
+		document.getElementById("error").innerHTML="number must be between 1 to 26, try again";
   } 
    function create_btns(result)
    {
@@ -185,31 +188,8 @@ button.addEventListener("click",function(e){
 
 $("#bt").on("click",function(e)
 {
-	e.preventDefault();
-	$.ajax({
-		"type":"GET",
-        "url":"index2.php",
-        "data":{"db":""},
-        "beforeSend":function(){
-          console.log("sending to server...");
-			
-		},
-        "success":function(response){
-				
-            console.log("server replayed");
-			var $db =JSON.parse(response);
-			$(".database").append("<table> <tr> <th>type</th> <th>targer</th> <th>date</th> </tr>");
-			
-			for($i=0;$i<$db.length;$i++){	
-			  	
-			$(".database").append("<tr> <td>"+  $db[$i].type+ "</td> <td>"+ $db[$i].targer +"</td> <td>"+
-              			$db[$i].date +"</td></tr></table>");
-			 
-			  
-			}
-     
-	}
-})
+	
+
 						
         }		
 				 
